30f77ca7f0819a8df318fdf630d75ecfd196a02b
MMyeong <ilikerunning@naver.com>
2024-07-03 22:19:58 +0900

    feat: auth, group 앱라우터 구성

---
+++ app/(site)/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = async () => {
+  return (
+    <div className=" w-full h-[500px] flex justify-center items-center">
+      메인 화면
+      <br />
+      <br />
+      1. 로그인 된 경우 - user 의 그룹 정보 받아서 특정 그룹
+      페이지(group/[id])로 리다이렉트
+      <br />
+      2. 로그인 안된 경우 - auth/login 으로 리다이렉트 하기
+    </div>
+  );
+};
+
+export default page;
---
+++ app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = async () => {
+  return (
+    <div className=" w-full h-[500px] flex justify-center items-center">
+      카카오톡 로그인 버튼
+    </div>
+  );
+};
+
+export default page;
