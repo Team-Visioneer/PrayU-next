918cdd48c9a5f050de53d1f8aac124aa326fda31
MMyeong <ilikerunning@naver.com>
2024-07-04 00:25:13 +0900

    chore: browser 대소문자 수정

--- lib/supabase/BrowserClient.ts
+++ lib/supabase/BrowserClient.ts
@@ -1,8 +1,9 @@
 import { createBrowserClient } from "@supabase/ssr";
 import { Database } from "@/types/tableSchema";
 
-export const createSupabaseBrowserClient = () =>
+export const createSupabaseBrowserClient = () => {
   createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+};
