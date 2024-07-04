8eeb6be0e72a2c2ba5fa3a3ff2dc3de8544d8f7b
mmyeong <82504981+myeongjunkim@users.noreply.github.com>
2024-07-04 11:15:02 +0900

    feat: auth, group 앱라우터 구성 (#3)
    * feat: auth, group 앱라우터 구성
    
    * chore: create -> new 로 변경
    
    * chore: route.ts 오타 수정

---
+++ app/group/new/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = async () => {
+  return (
+    <div className=" w-full h-[500px] flex justify-center items-center">
+      그룹 생성 페이지
+    </div>
+  );
+};
+
+export default page;
